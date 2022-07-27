3340c80c392fd22441c5b357f0ea7b7012a66f25
patrickwheatley2004 <patrickwheatley2004@gmail.com>
2022-07-27 11:17:35 +0800

    Add project files.

---
+++ WeekTwoNew/FormFileIO.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WeekTwoNew
+{
+    partial class FormFileIO
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lbxOutput = new System.Windows.Forms.ListBox();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.tbxPhone = new System.Windows.Forms.TextBox();
+            this.tbxPosition = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lbxOutput
+            // 
+            this.lbxOutput.FormattingEnabled = true;
+            this.lbxOutput.Location = new System.Drawing.Point(12, 41);
+            this.lbxOutput.Name = "lbxOutput";
+            this.lbxOutput.Size = new System.Drawing.Size(120, 212);
+            this.lbxOutput.TabIndex = 1;
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(139, 41);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(121, 20);
+            this.tbxName.TabIndex = 2;
+            // 
+            // tbxPhone
+            // 
+            this.tbxPhone.Location = new System.Drawing.Point(138, 67);
+            this.tbxPhone.Name = "tbxPhone";
+            this.tbxPhone.Size = new System.Drawing.Size(121, 20);
+            this.tbxPhone.TabIndex = 3;
+            // 
+            // tbxPosition
+            // 
+            this.tbxPosition.Location = new System.Drawing.Point(138, 93);
+            this.tbxPosition.Name = "tbxPosition";
+            this.tbxPosition.Size = new System.Drawing.Size(121, 20);
+            this.tbxPosition.TabIndex = 4;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(138, 119);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(121, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(138, 148);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(121, 23);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // FormFileIO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbxPosition);
+            this.Controls.Add(this.tbxPhone);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.lbxOutput);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "FormFileIO";
+            this.Text = "Form File IO";
+            this.Load += new System.EventHandler(this.FormFileIO_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lbxOutput;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.TextBox tbxPhone;
+        private System.Windows.Forms.TextBox tbxPosition;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ WeekTwoNew/FormFileIO.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeekTwoNew
+{
+    public partial class FormFileIO : Form
+    {
+        public FormFileIO()
+        {
+            InitializeComponent();
+        }
+
+        static int max = 12; // row
+        static int attributes = 3; // column
+        private string[,] ArrayInfo = new string[max, attributes];
+        int ptr = 0;
+
+        private void DisplayList()
+        {
+            lbxOutput.Items.Clear();
+            string rec = "";
+            for (int x = 0; x < ptr; x++)
+            {
+                rec = ArrayInfo[x, 0] + " " + ArrayInfo[x, 1];
+                lbxOutput.Items.Add(rec);
+            }
+        }
+
+        private void clearTextBoxes()
+        {
+            tbxName.Clear();
+            tbxPhone.Clear();
+            tbxPosition.Clear();
+            tbxName.Focus();
+        }
+
+        private void FormFileIO_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (ptr < max)
+            {
+                try
+                {
+                    ArrayInfo[ptr, 0] = tbxName.Text;
+                    ArrayInfo[ptr, 1] = tbxPhone.Text;
+                    ArrayInfo[ptr, 2] = tbxPosition.Text;
+                    ptr++;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("ERROR: " + ex);
+                }
+            }
+            else
+            {
+                MessageBox.Show("The array is full");
+            }
+            clearTextBoxes();
+            DisplayList();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (var stream = File.Open("employee.bin", FileMode.Create))
+            {
+                using (var writer = new BinaryWriter(stream, Encoding.UTF8, false)) // the false means that the system is asking if you want to leave the file open or not. 
+                {
+                    for (int x = 0; x < ptr; x++)
+                    {
+                        for (int y = 0; y < attributes; y++)
+                        {
+                            writer.Write(ArrayInfo[x, y]);
+                        }
+                    }
+                }
+            }
+            MessageBox.Show("All the datate has been saved!");
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("employee.bin"))
+            {
+                using (var stream = File.Open("employee.bin", FileMode.Open))
+                {
+                    using (var reader = new BinaryReader(stream, Encoding.UTF8, false)) // the false means that the system is asking if you want to leave the file open or not. 
+                    {
+                        int x = 0;
+                        //Array.Clear(ArrayInfo, 0, max);
+                        while(stream.Position < stream.Length)
+                        {
+                            for (int y = 0; y < attributes; y++)
+                            {
+                                ArrayInfo[x, y] = reader.ReadString();
+                            }
+                            x++;
+                        }
+                        ptr = x;
+                    }
+                }
+                DisplayList();
+            }
+            else
+            {
+                MessageBox.Show("The file you tried to open wasn't found.");
+            }
+        }
+    }
+}
